a5b31487ecfaa27aa3e2f3f3cde186b3026c75d1
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2024-02-05 06:19:20 +0530

    Time: 136 ms (5.81%) | Memory: 16.8 MB (56.78%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        i = 0
+
+        resultState = False
+        while i < len(arr) - 1:
+            j = i + 1
+            while j <= len(arr) - 1:
+                conditioncheck = self.checkCondtions(arr[i], arr[j])
+                print(
+                    "i -> "
+                    + str(arr[i])
+                    + "--j -> "
+                    + str(arr[j]) + "--check ->" + str(conditioncheck)
+                )
+                if conditioncheck:
+                    resultState = True
+                    break
+                j += 1
+                if resultState is True:
+                    break
+            if resultState is True:
+                break
+            i += 1
+
+        return resultState
+
+    def checkCondtions(self, i: int, j: int) -> bool:
+        return (i != j and (i == 2 * j or j == 2*i)) or (i is 0 and j is 0)
