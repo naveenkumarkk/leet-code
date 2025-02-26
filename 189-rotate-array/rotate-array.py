380f5b553198701d5107403f5c8ff104ce043663
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-02-26 20:22:38 +0200

    Time: 1015 ms (9.03%) | Memory: 25.6 MB (74.66%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -8,4 +8,11 @@ class Solution:
             removedElement = nums.pop()
             nums.insert(0,removedElement)
 
-        
+
+# def rotate(nums: List[int], k: int) -> None:
+#     """
+#     Rotates the array to the right by k steps.
+#     This solution runs in O(n) time and O(1) space.
+#     """
+#     k %= len(nums)  # Handle cases where k is larger than length of nums
+#     nums[:] = nums[-k:] + nums[:-k]  # Slice and concatenate
