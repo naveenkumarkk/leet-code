ca02363ed679125496fd0f1b1614bc337ca5dfc9
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-04 00:31:42 +0200

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.08%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,26 +1,23 @@
 class Solution:
-    # Thought Process:
-    # multiply with its units place
-    # and sum it up
-    # add 1
-    # divide the elemts to get it its
-
     def plusOne(self, digits: List[int]) -> List[int]:
-        
-        i = len(digits) - 1
-        multiplier = 1
-        total = 0
-        resultArray = []
-
-        while i >= 0:
-            digitNumber = digits[i] * multiplier
-            total += digitNumber
-            multiplier *= 10
-            i -= 1
-
-        total += 1
-
-        for ch in str(total):
-            resultArray.append(int(ch))
-
-        return resultArray
+    
+        total=0
+        init_multiply = 1
+        result = []
+        if digits[-1] > 8:
+            for i in range(len(digits)-1,-1,-1):
+                digits[i] *= init_multiply
+                print(digits[i])
+                init_multiply *=10
+                total += digits[i]
+            
+            total +=1
+            total = str(total)
+            
+            for j in total:
+                result.append(int(j))
+            
+            return result
+        else:
+            digits[-1] += 1
+            return digits
