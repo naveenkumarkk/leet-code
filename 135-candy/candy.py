67cfb3414bebf73b80bcb13ae494ff9c2d582a27
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-04 20:58:27 +0200

    Time: 19 ms (43.03%) | Memory: 19.9 MB (70.70%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -5,11 +5,9 @@ class Solution:
         for i in range(len(ratings)-1):
             if ratings[i] < ratings[i+1]:
                 shadown_list[i+1]=shadown_list[i]+1
-
-        print(shadown_list)
+                
         for i in range(len(ratings)-2,-1,-1):
            if ratings[i] > ratings[i+1] :
                 shadown_list[i] = max(shadown_list[i], shadown_list[i + 1] + 1)
-        print(shadown_list)
-    
+        
         return sum(shadown_list)
