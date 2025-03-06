65a2ea4763352389d5a6df0ccb0e40ed1df414c2
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-06 23:12:51 +0200

    Time: 2 ms (5.39%) | Memory: 17.6 MB (88.49%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -5,11 +5,12 @@ class Solution:
         start = 0
         end = 0
         sarray = []
+        s.strip()
         for i in range(len(s)):
             if (s[i] == ' ' or i == len(s)-1) :
                 end = i+1
                 sarray.append(s[start:end])
-                start=i+1
+                start=end
         
         for j in range(len(sarray)-1,-1,-1):
             if sarray[j] != ' ' and sarray[j] !='' :
