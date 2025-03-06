c25d3281d328da1bd1f3e023d6361c2384f6931a
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-06 23:54:35 +0200

    Time: 3 ms (28.26%) | Memory: 17.7 MB (78.74%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,36 +1,17 @@
-# Thought process
-# get the minimum string length of the given array elements
-# check for given string index all the elements have same char
-# if yes add the character to the empty string
-# if the character is a mismatch store it in result string
-
-
 class Solution:
+    
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        arrayLength = len(strs)
-        resultString = ""
-        minimumStringIndex = 0
-        minLength = len(strs[0])
-        index = 0
-
-        for i in range(arrayLength):
-            if minLength > len(strs[i]):
-                minLength = len(strs[i])
-                minimumStringIndex = i
-
-        resultString = strs[minimumStringIndex]
-
-        strs = set(strs)
-        strs.discard(minimumStringIndex)
-        strs = list(strs)
-
-        while index < len(strs) and len(resultString) != 0:
-            elementString = strs[index]
-            if elementString[0:minLength] != resultString:
-                index = 0
-                resultString = resultString[:-1]
-                minLength = len(resultString)
-            else:
-                index += 1
-
-        return resultString
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        for word in strs[1:]:
+            i = 0
+            while i < len(prefix) and i < len(word) and prefix[i] == word[i]:
+                i += 1
+            prefix = prefix[:i]
+            
+            if not prefix:  
+                break
+                
+        return prefix
