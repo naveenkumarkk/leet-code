19ca025ffeb8f6ba3cb7059200b648cf0ba90008
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-07 20:42:44 +0200

    Time: 0 ms (100.00%) | Memory: 18.1 MB (24.28%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,18 +1,7 @@
-# Thought Process:
-# Initialise left Pointer at the begining of the array
-# Initialise roght pointer at the end of the array
-# swap the pointer values until it both equal
 class Solution:
     def reverseWords(self, s: str) -> str:
-        stringArray = s.strip().split()
-        left , right = 0,len(stringArray)-1
-
-        while left<=right:
-            temp = stringArray[left]
-            stringArray[left] = stringArray[right]
-            stringArray[right] = temp
-            left+=1
-            right-=1
-        
-        return ' '.join(stringArray)
+        s.strip()
+        text = s.split()
+        text.reverse()
+        return " ".join(text)
 
