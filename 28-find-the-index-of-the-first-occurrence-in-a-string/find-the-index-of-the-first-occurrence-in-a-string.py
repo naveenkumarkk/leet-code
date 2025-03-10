bfcb4a86f96764964ba84f4736bc14e5d09010cf
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-10 22:06:11 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.64%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,9 @@
-# Thought Process
-# we need to search given input word is present in the given line
-# If nothing is present return -1
-# if yes return the start of the index
-# so the given entire word should be present in a continuous
-
-# construct a string till the length of the input string
-
-
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-            if not needle: return 0
-            n_len = len(needle)
-            for i in range(len(haystack) - n_len + 1):
+        n_len = len(needle)
+        for i in range(len(haystack) - n_len + 1):
                 if haystack[i:i+n_len] == needle: return i
-            return -1
+        return -1
 
+        
+       
