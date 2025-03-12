bb64d87632d80967d13a718f21d2573cee537001
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-03-12 21:44:48 +0200

    Time: 3 ms (81.78%) | Memory: 18.6 MB (59.81%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,29 +1,17 @@
-# Thought Process:
-# Given array is sorted
-# I need to add two elements which equal to the given target in an array and return those indexes
-# I can omit the array elements which are grater then the target
-# Edge Case:But if there are only two elements in an array I should consider 0 as well
-# I need to iterate through each index element and add it to the next all elements and see if the sum is
-# is equal to the target
-
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        if not numbers:
-            return []
-
-        left, right = 0, len(numbers) - 1
-
-        while left < right:
-            current_sum = numbers[left] + numbers[right]
-            if current_sum == target:
-                return [left + 1, right + 1]  # Adjusting indexes to 1-based indexing
-            elif current_sum < target:
-                left += 1
-            else:
-                right -= 1
+        
+        first_pointer, second_pointer = 0,len(numbers)-1
+        listLen = len(numbers)
+        res = []
+        tempSum = 0
+        while  first_pointer < second_pointer :
+            tempsum = numbers[first_pointer] + numbers[second_pointer]
+            if tempsum == target:
+                return [first_pointer +1,second_pointer+1]
+            elif tempsum < target:
+                first_pointer +=1
+            else :
+                second_pointer -= 1
 
         return []
-
-    def checkSum(self, num1: int, num2: int, target: int):
-        return target == (num1 + num2)
