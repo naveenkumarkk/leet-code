24fe6c1c308ed7e5af955d698e3449b6ed4bccfb
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-06-03 12:29:16 +0300

    Time: 1 ms (89.59%) | Memory: 21.6 MB (52.86%) - LeetSync

--- 835-linked-list-components/linked-list-components.py
+++ 835-linked-list-components/linked-list-components.py
@@ -5,27 +5,44 @@
 #         self.next = next
 class Solution:
     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
-        counter = 0
-        checkValue = 0
-        while head is not None:
-            value = head.val
-            head = head.next
-            found = False
+        # Logic 1
+        # counter = 0
+        # checkValue = 0
+        # while head is not None:
+        #     value = head.val
+        #     head = head.next
+        #     found = False
            
-            for n in nums:
-                if n == value:
-                    checkValue +=1
-                    found = True
-                    break
+        #     for n in nums:
+        #         if n == value:
+        #             checkValue +=1
+        #             found = True
+        #             break
             
-            if found == False and checkValue > 0:
-                checkValue = 0
-                counter += 1
+        #     if found == False and checkValue > 0:
+        #         checkValue = 0
+        #         counter += 1
         
-        if checkValue > 0:
-            counter += 1
+        # if checkValue > 0:
+        #     counter += 1
         
+        # return counter
+
+        # Logic 2
+        num_set = set(nums)
+        counter = 0
+        in_component = False
+
+        while head:
+            if head.val in num_set:
+                if not in_component:
+                    counter +=1
+                    in_component = True
+            else:
+                in_component = False
+            head = head.next
         return counter
+
             
             
                 
