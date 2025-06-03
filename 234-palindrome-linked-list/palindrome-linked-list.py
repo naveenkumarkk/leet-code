be14ed25d8d65d493f2c51be4d031a3f7288c188
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-06-03 11:42:30 +0300

    Time: 28 ms (70.71%) | Memory: 34.8 MB (82.44%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,15 +5,40 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        data = ''
-        while head is not None:
-            data = data + str(head.val)
-            head = head.next
+        # Logic 1
+        # data = ''
+        # while head is not None:
+        #     data = data + str(head.val)
+        #     head = head.next
         
         # lenData = len(data) - 1
         # reversedData = ''
         # while lenData >= 0:
         #     reversedData = reversedData + str(data[lenData])
         #     lenData -=1
-        return data[::-1] == data
+        # return data[::-1] == data
+
+        # Logic 2
+        turtle = rabbit = head
+        while rabbit and rabbit.next:
+            turtle = turtle.next
+            rabbit = rabbit.next.next
+        
+        prev = None
+        while turtle:
+            next_node = turtle.next
+            turtle.next= prev
+            prev = turtle
+            turtle = next_node
+        
+        first_half,second_half = head,prev
+
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half =  first_half.next
+            second_half = second_half.next
+
+        return True
+
             
