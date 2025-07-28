c9898c0907dfc2147dc672c2f302f4657b71084e
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-07-28 17:05:57 +0300

    Time: 3 ms (64.72%) | Memory: 17.9 MB (19.71%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,29 +1,20 @@
+
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        vowels_dict = {
-            'a':0,
-            'e':0,
-            'i':0,
-            'o':0,
-            'u':0
-        }
-        constant_dict = {}
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        vowel_count = defaultdict(int)
+        consonant_count = defaultdict(int)
 
         for char in s:
-            if char in vowels_dict:
-                vowels_dict[char] += 1
+            if char in vowels:
+                vowel_count[char] += 1
             else:
-                if char in constant_dict:
-                    constant_dict[char] += 1
-                else:
-                    constant_dict[char] = 1
-        max_value = 0
-        if any(constant_dict):
-            max_value += max(constant_dict.values())
+                consonant_count[char] += 1
 
-        if any(vowels_dict):
-            max_value += max(vowels_dict.values())
-        
-        return max_value
+        max_sum = 0
+        if consonant_count:
+            max_sum += max(consonant_count.values())
+        if vowel_count:
+            max_sum += max(vowel_count.values())
 
-
+        return max_sum
