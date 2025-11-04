703e0f0bd0831e79892a04d98fd90cf5128e8f23
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-11-04 09:31:51 +0200

    Time: 71 ms (23.02%) | Memory: 17.6 MB (89.61%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -0,0 +1,15 @@
+class Solution:
+    def is_nice(self, s: str) -> bool:
+        return len(set(s.lower())) == (len(set(s)) // 2)
+
+    def longestNiceSubstring(self, s: str) -> str:
+        window_size = len(s)
+
+        while window_size:
+            for i in range(len(s) - window_size + 1):
+                substring = s[i : i + window_size]
+                if self.is_nice(substring):
+                    return substring
+
+            window_size -= 1
+        return ""
