e9c70a680e31b06650fb039d963b22cf4a96b99a
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-11-05 10:23:55 +0200

    Time: 0 ms (100.00%) | Memory: 17.9 MB (45.18%) - LeetSync

--- 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.py
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.py
@@ -1,10 +1,19 @@
+from collections import deque
+
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
+        s = str(num)
         count = 0
-        string_num = str(num)
-        for R in range(len(string_num)-k+1):
-            sub_num = int (string_num[R:R+k])
-            if sub_num > 0 and num % sub_num == 0:
-               count+=1  
+        window = deque()
 
+        for ch in s:
+            window.append(ch)
+
+            if len(window) > k:
+                window.popleft()
+
+            if len(window) == k:
+                sub_num = int("".join(window))
+                if sub_num>0 and num % sub_num ==0:
+                    count+=1
         return count
