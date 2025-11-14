04428d33ed2047aca0356e234b2465948ae71788
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-11-14 10:22:46 +0200

    Time: 11 ms (81.02%) | Memory: 17.6 MB (97.89%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.py
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.py
@@ -1,28 +1,50 @@
+class Node:
+    def __init__(self,val = 0, next = None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
         operation_count = 0
+        dummy = Node()
+        current = dummy
 
-        while len(nums) > 1:
+        for val in nums:
+            current.next = Node(val)
+            current = current.next
+
+        head = dummy.next
+
+        while head:
             is_sorted = True
+            curr = head
 
-            for i in range(len(nums)-1):
-                if nums[i] > nums[i+1]:
+            while curr and curr.next:
+                if curr.val > curr.next.val:
                     is_sorted = False
                     break
-
+                curr = curr.next
+            
             if is_sorted:
                 break
             
             operation_count += 1
 
             minimum_sum = float('inf')
-            minimum_index = -1
-
-            for i in range(len(nums)-1):
-                if minimum_sum > (nums[i] + nums[i+1]):
-                    minimum_sum = nums[i] + nums[i+1]
-                    minimum_index = i
-            nums[minimum_index:minimum_index+2] = [minimum_sum]
-
+            minimum_node = None
+            curr = head
+            while curr and curr.next:
+                node_sum = curr.val + curr.next.val
+                if node_sum < minimum_sum:
+                    minimum_sum = node_sum
+                    minimum_node = curr
+                curr = curr.next
+            
+            if minimum_node:
+                minimum_node.val = minimum_sum
+                node_to_remove = minimum_node.next
+                minimum_node.next = node_to_remove.next
+            else:
+                break
 
         return operation_count
