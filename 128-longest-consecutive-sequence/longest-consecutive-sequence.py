c1dc3900215be673ca026cd3a38a1f3d4ba5aa61
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2026-03-08 16:38:22 +0200

    Time: 66 ms (17.54%) | Memory: 36.8 MB (37.33%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,25 +1,52 @@
 class Solution:
-    def checkNumberIsStartSequence(self,number:int,nums:set) ->bool:
-        return number - 1 not in nums
 
-    def checkNumberHasNext(self,number:int,nums:set) ->bool:
-        return number + 1 in nums
+    def checkIfNumHasNext(self,n,nums:set)->bool:
+        return n + 1 in nums
+
+    def checkIfNumisStartSequence(self,n,nums:set)->bool:
+        return n - 1 not in nums
 
     def longestConsecutive(self, nums: List[int]) -> int:
+        maxLength = 0
         if len(nums) == 0:
             return 0
-
-        set_array = set(nums)
-        count = 0
-        max_count = 0
-
-        for n in set_array:
-            if self.checkNumberIsStartSequence(n,set_array):
+        nums = set(nums)
+        for n in nums:
+            if self.checkIfNumisStartSequence(n,nums):
                 length = 1
                 current = n
-                while self.checkNumberHasNext(current,set_array):
-                    current += 1
+                while self.checkIfNumHasNext(current,nums):
                     length += 1
-                
-                max_count = max(max_count,length)
-        return max_count
+                    current += 1
+                maxLength = max(maxLength,length)
+
+        return maxLength
+
+        # If Input is positive the following code works
+        # hashMap = {}
+        # if len(nums) == 0:
+        #     return 0
+        # maxEle = max(nums)
+        # bucket = [0] * (maxEle + 1)
+
+        # for i in range(len(nums)):
+        #     if nums[i] in hashMap:
+        #         hashMap[nums[i]]+= 1
+        #     else:
+        #         hashMap[nums[i]] = 1
+        
+        # for key,value in hashMap.items():
+        #     bucket[key] = value
+
+        # maxCount = 0
+        # count = 0
+        
+        # for index,value in enumerate(bucket):
+
+        #     if value == 0:
+        #         maxCount = max(maxCount,count)
+        #         count = 0
+        #     else:
+        #         count += 1
+        # maxCount = max(maxCount,count)
+        # return maxCount
