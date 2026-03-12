7bb19f2ef5b0e7d47fd131c483fcfedf17252430
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2026-03-12 16:39:06 +0200

    Time: 20 ms (10.98%) | Memory: 21 MB (95.61%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,27 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # trapped_water = 0
+        arrayLen = len(height)
+        maxLeft = [0] * arrayLen
+        maxRight = [0] * arrayLen
 
-        # for i in range(1,len(height)):
-        #     leftMax = max(height[:i])
-        #     rightMax = max(height[i:]) 
-        #     trapped_water += max(min(leftMax,rightMax) - height[i],0)
-
-        # return trapped_water
-        left,right = 0,len(height)-1
-        leftMax,rightMax = height[left],height[right]
-        res = 0
-
-        while left < right:
-            if leftMax < rightMax:
-                left += 1
-                res += max(leftMax - height[left],0)
-                leftMax = max(leftMax,height[left])
-            else:
-                right -= 1
-                res += max(rightMax - height[right],0)
-                rightMax = max(rightMax,height[right])
-        return res
+        maxLeftValue ,maxRightValue = height[0],height[arrayLen-1]
+        maxWater = 0
+        for i in range(1,arrayLen):
+            maxLeftValue = max(maxLeftValue,height[i-1])
+            maxLeft[i] = maxLeftValue
+            
         
+        for i in range(arrayLen-2,-1,-1):
+            maxRightValue = max(maxRightValue,height[i+1])
+            maxRight[i] = maxRightValue
 
-                
+        for i in range(arrayLen):
+            water = min(maxLeft[i],maxRight[i]) - height[i]
+            maxWater += max(water,0)
+        return maxWater
+
