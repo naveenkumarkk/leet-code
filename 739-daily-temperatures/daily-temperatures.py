48ed7b4db3a38dddb7cbec397c47a7c60c307616
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2026-03-30 21:45:08 +0300

    Time: 123 ms (21.64%) | Memory: 36.1 MB (7.10%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,18 +1,5 @@
-from collections import deque
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
-        # res = []
-        # for i in range(len(temperatures)):
-        #     for j in range(i,len(temperatures)):
-        #         if temperatures[i] < temperatures[j]:
-        #             res.append(j-i)
-        #             break
-        #         elif j == len(temperatures) - 1:
-        #             res.append(0)
-        # return res
-        
         res = [0] * len(temperatures)
         stack = []
 
@@ -21,5 +8,4 @@ class Solution:
                 temp,stackKey = stack.pop()
                 res[stackKey] = key - stackKey
             stack.append([value,key])
-
         return res
